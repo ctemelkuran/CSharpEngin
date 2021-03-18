c9f486f2fdf4294875bd38f4186caf0550cf9364
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-18 10:32:15 +0300

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 10;
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
