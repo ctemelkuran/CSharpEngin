4483cb5cbfe52f66b1682f9ff4a83ee2d5d29ce9
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-18 11:40:50 +0300

    params, overloading

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -13,11 +13,17 @@ namespace Methods
 
             // var result = Add(20);
 
-            int number1 = 200;
-            int number2 = 50;
-            var result2 = Subtract(ref number1, number2);
-            Console.WriteLine(result2);
-            Console.WriteLine(number1);
+            //int number1; // out ile değer atamaya gerek yok, ref'te atanmalı
+            //int number2 = 50;
+            //var result2 = Subtract(out number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+
+            //Console.WriteLine(Multiply(10, 2));
+            //Console.WriteLine(Multiply(10, 2, 10));
+
+            //Console.WriteLine(Add(2, 3, 4, 5, 6, 7));
+
             Console.ReadLine();
         }
 
@@ -27,10 +33,29 @@ namespace Methods
             return result;
         }
 
-        static int Subtract(ref int number1, int number2)
+        //değer methodta değişebilir ve bunu kabul ediyorsan ref veya out keyword
+        static int Subtract(out int number1, int number2)
         {
-            number1 = 100; //değer methodta değişebilir ve bunu kabul ediyorsan ref keyword
+            number1 = 100; // out kullanırsan method içinde set edilmeli
             return number1 - number2;
+        }
+
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        // method overloading
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+
+        //params ile methoda istenilen miktarda parametre girilebilir
+        static int Add(int specialNumber, params int[] numbers) // params methodun son parametresi olmalı
+        {
+            Console.WriteLine(specialNumber);
+            return numbers.Sum();
         }
     }
 }
