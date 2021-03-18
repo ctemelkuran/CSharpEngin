19ebec614bf51f02286b52d0e619e76db7fe1df6
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-18 10:55:12 +0300

    iç içe if

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,22 +10,40 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 10;
-            if (number == 10)
+            var number = 92;
+            //if (number == 10)
+            //{
+            //    Console.WriteLine("Number is 10");
+            //}
+            //else if (number == 20)
+            //{
+            //    Console.WriteLine("Number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not 10 or 20");
+            //}
+
+            //// Single-line if
+            //Console.WriteLine(number == 12 ? "Number is 12" : "Number is not 12");
+            
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("Number şs 10");
+            //    default:
+            //        Console.WriteLine("Not 10 or 20");
+            //        break;
+            //}
+
+            if (number <100)
             {
-                Console.WriteLine("Number is 10");
-            }
-            else if (number == 20)
-            {
-                Console.WriteLine("Number is 20");
-            }
-            else
-            {
-                Console.WriteLine("Not 10 or 20");
+                if (number >= 90 && number<95)
+                {
+                    Console.WriteLine("Harf notu BA");
+                }
             }
 
-            // Single-line if
-            Console.WriteLine(number == 12 ? "Number is 12" : "Number is not 12");
 
             Console.ReadLine();
         }
