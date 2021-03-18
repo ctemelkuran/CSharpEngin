e7073df1a8fffb82b4883210197fc3f73d6cb673
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-18 13:13:11 +0300

    Multi-dimension array

---
+++ Arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] students = new string[1];
+            //students[1] = "Derin";
+
+            string[] students2 = { "Derin", "Fatma", "asd" };
+
+            foreach (var student in students2)
+            {
+                //Console.WriteLine(student);
+            }
+            
+
+            //multi-dimensional array
+            string[,] catType = new string[2, 3]
+            {
+                {"Zeytin", "Smokin","Female" },
+                {"Arven", "3 color","Female" }
+            };
+
+            // belirtilen dimension uzunluğu
+            for (int i = 0; i < catType.GetLength(0); i++)
+            {
+                //belirtilen dimensionın son elemanın indexi
+                for (int j = 0; j <= catType.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(catType[i, j]);
+                }
+                Console.WriteLine("........");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
