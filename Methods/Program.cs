8ff1a63d85668cc980e7dbc58738f082d2e31664
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-18 11:27:05 +0300

    ref keyword

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -11,9 +11,13 @@ namespace Methods
         static void Main(string[] args)
         {
 
-            var result = Add(20);
-            Console.WriteLine(result);
+            // var result = Add(20);
 
+            int number1 = 200;
+            int number2 = 50;
+            var result2 = Subtract(ref number1, number2);
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
             Console.ReadLine();
         }
 
@@ -21,6 +25,12 @@ namespace Methods
         { // default değer metodun en sonunda konur
             var result = number1 + number2;
             return result;
+        }
+
+        static int Subtract(ref int number1, int number2)
+        {
+            number1 = 100; //değer methodta değişebilir ve bunu kabul ediyorsan ref keyword
+            return number1 - number2;
         }
     }
 }
