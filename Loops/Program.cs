34bd2203c1a3b35cc005638613cd7943c2ff5351
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-18 15:17:20 +0300

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Count();
+            //DoWhile();
+            //ForEachLoop();
+            //Console.Write("Enter a number to check prime: ");
+            //int input = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(IsPrimeNumber(input) == true ? "Number is prime" : "Not prime");
+
+            Console.ReadLine();
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number%i==0)
+                {
+                    result = false;
+                    
+                }
+            }
+            return result;
+        }
+
+
+        private static void ForEachLoop()
+        {
+            string[] students = { "Anan", "Fatma", "Çığır" };
+
+            //tabloda gezinmek, enumarable nesne gezmek
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        private static void doWhile()
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine("Number is {0}", number);
+                number--;
+            } while (number >= 11);
+        }
+
+        private static void Count()
+        {
+            for (int i = 100; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
