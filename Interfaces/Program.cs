a8f948cc07d0696267d95cc67a6222917b2fe657
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-20 16:53:15 +0300

    Person manger

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,32 +10,49 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Çığır",
+                Address = "Izmir"
+            };
+
+            manager.Add(customer);
         }
 
         //Interface: temel nesne ile bütün nesneleri ondan implemente etmek için.
         interface IPerson //abstract, soyut nesne
         {
             int Id { get; set; }
-            int FirstName { get; set; }
-            int LastName { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
         }
 
         class Customer : IPerson
         {
             public int Id { get; set; }
-            public int FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-            public int LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
             public string Address { get; set; }
         }
 
         class Student : IPerson
         {
             public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-            public int FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-            public int LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
             public string Departmant { get; set; }
         }
 
+        class PersonManager
+        {
+            public void Add(Customer customer)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+        }
     }
 }
