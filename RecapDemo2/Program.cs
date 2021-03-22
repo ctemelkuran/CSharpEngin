7a6e07ee92b6f192f68bf4e792e6004a700f764a
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-22 14:16:19 +0300

    db and filelogger

--- RecapDemo2/Program.cs
+++ RecapDemo2/Program.cs
@@ -10,14 +10,43 @@ namespace RecapDemo2
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new DatabaseLogger();
+            customerManager.Add();
+
+            Console.Read();
         }
     }
 
     class CustomerManager
     {
+        public ILogger Logger { get; set; }
         public void Add()
         {
+            Logger.Log();
             Console.WriteLine("Customer added!");
         }
+    }
+
+    //Bir class ın bir base class ı olmalı, örndeğin loglama işlemi ise
+    class DatabaseLogger: ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!");
+        }
+    }
+
+    class FileLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
     }
 }
