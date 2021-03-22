b4d72454940169e279c04da8b3fd2171499245c4
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-22 14:08:15 +0300

    Recapdemo2

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer added!");
+        }
+    }
+}
