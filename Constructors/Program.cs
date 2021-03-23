73023a27fc88f7f034b11c15033c48cb9497a85f
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-23 10:52:01 +0300

    constructurs

---
+++ Constructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        public CustomerManager()
+        {
+
+        }
+        public void List()
+        {
+            Console.WriteLine("Listed!");
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+    }
+}
