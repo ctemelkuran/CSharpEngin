df6bfbc8082692e1398cc1794cfc4188b91b9509
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-23 10:21:17 +0300

    accessmodifiers

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Customer
+    {
+        // default olarak private dır tanımlandığı blok içinde geçerli
+        int id;
+    }
+
+    class Student
+    {
+        public void Save()
+        {
+            Customer customer = new Customer();
+            
+        }
+    }
+}
