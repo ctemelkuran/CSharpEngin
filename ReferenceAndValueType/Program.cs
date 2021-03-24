5100696744c53f7a2cd1f083b89d54fadcb7ef86
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-24 11:39:28 +0300

    reference and value types

---
+++ ReferenceAndValueType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndValueType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //abstracts like: class and string are reference type
+            // bool, double, int value type
+            int number1 = 10;
+            int number2 = 20;
+            number2 = number1;
+            number1 = 30;
+            Console.WriteLine(number2);
+
+            Console.ReadLine();
+        }
+    }
+}
