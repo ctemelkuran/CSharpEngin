e493e837779e14ab130522ccdef887c67ce79f95
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-25 17:32:41 +0300

    exceptions project

---
+++ Exceptions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3] { "Çığır", "Fatma","Zeytin"};
+            students[3] = "Arven";
+
+            Console.ReadLine();
+        }
+    }
+}
