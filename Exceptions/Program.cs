78e67ee3fb99febc980927dfc9b7ac3a7ddd04d1
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-25 17:46:06 +0300

    try catch exception

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,12 +10,42 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            //ExceptioIntro();
+
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFoundException exception)
+            {
+
+                Console.WriteLine(exception.Message);
+            }
+            
+            Console.ReadLine();
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string> { "Çığır", "Fatma", "Zeytin" };
+            if (!students.Contains("Arven"))
+            {
+                throw new RecordNotFoundException();
+            }
+            else
+            {
+                Console.WriteLine("Record found");
+            }
+        }
+
+        private static void ExceptioIntro()
+        {
             try
             {
                 string[] students = new string[3] { "Çığır", "Fatma", "Zeytin" };
                 students[3] = "Arven";
             }
-            catch(DivideByZeroException ex)
+            catch (DivideByZeroException ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -23,11 +53,8 @@ namespace Exceptions
             {
                 Console.WriteLine(exception.Message);
                 //Console.WriteLine(exception.InnerException);
-                
-            }
-            
 
-            Console.ReadLine();
+            }
         }
     }
 }
