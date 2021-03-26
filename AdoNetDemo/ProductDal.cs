cf470d30ec6c2581c9d129463209f8e65b9bdbfd
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-26 15:42:43 +0300

    Productları Form1'e çağırdık

--- AdoNetDemo/ProductDal.cs
+++ AdoNetDemo/ProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,25 @@ namespace AdoNetDemo
 { // Normalde kurumsal mimaride IProductDal 'dan çekiyor olururuz
     public class ProductDal //Data Access Layer veya Object
     {
-        public List<Product> GetAll()
+        public DataTable GetAll()
         {
+            //integrated security=true veri tabanına direkt local erişim sağlar, bilgisayara login olmak yeterli
+            // uzaktaki veritabanı için false yapılır kullanıcı girilir 
             SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb; initial catalog = ETradeCSharpEngin; integrated security=true");
+            if (connection.State==ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            //sql komutu yazılır
+            SqlCommand sqlCommand = new SqlCommand("Select * from Products", connection);
+            //komut execute edilmeli
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+            return dataTable;
+             
         }
     }
 }
