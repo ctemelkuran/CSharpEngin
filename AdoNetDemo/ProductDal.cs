d15fd6bcca5751361f5c903bf03d2e8f97696eb9
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-26 15:26:35 +0300

    form, productdal, product created and connectted to sqldb

---
+++ AdoNetDemo/ProductDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{ // Normalde kurumsal mimaride IProductDal 'dan çekiyor olururuz
+    public class ProductDal //Data Access Layer veya Object
+    {
+        public List<Product> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb; initial catalog = ETradeCSharpEngin; integrated security=true");
+        }
+    }
+}
