03c87cf30daa1791932127a11be73b75e6241e25
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-26 15:46:18 +0300

    List<Product>, while reader.Read

--- AdoNetDemo/ProductDal.cs
+++ AdoNetDemo/ProductDal.cs
@@ -10,7 +10,7 @@ namespace AdoNetDemo
 { // Normalde kurumsal mimaride IProductDal 'dan çekiyor olururuz
     public class ProductDal //Data Access Layer veya Object
     {
-        public DataTable GetAll()
+        public List<Product> GetAll()
         {
             //integrated security=true veri tabanına direkt local erişim sağlar, bilgisayara login olmak yeterli
             // uzaktaki veritabanı için false yapılır kullanıcı girilir 
@@ -23,12 +23,39 @@ namespace AdoNetDemo
             SqlCommand sqlCommand = new SqlCommand("Select * from Products", connection);
             //komut execute edilmeli
             SqlDataReader reader = sqlCommand.ExecuteReader();
-            DataTable dataTable = new DataTable();
-            dataTable.Load(reader);
+
+            List<Product> products = new List<Product>();
+
+            while (reader.Read() // dataları okuyabildiğin sürece çalıştır
+            {
+
+            }
+
+
             reader.Close();
             connection.Close();
             return dataTable;
              
+        }
+        public DataTable GetAll2()
+        {
+            //integrated security=true veri tabanına direkt local erişim sağlar, bilgisayara login olmak yeterli
+            // uzaktaki veritabanı için false yapılır kullanıcı girilir 
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb; initial catalog = ETradeCSharpEngin; integrated security=true");
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            //sql komutu yazılır
+            SqlCommand sqlCommand = new SqlCommand("Select * from Products", connection);
+            //komut execute edilmeli
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            DataTable dataTable = new DataTable(); // DataTable nesnesi kullanılmaz istenmez
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+            return dataTable;
+
         }
     }
 }
