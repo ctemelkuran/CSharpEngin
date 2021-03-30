69ea77cbee817662dbea6a86a5e280fdd634b31c
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-03-30 20:59:07 +0300

    AttributeUsage(AttributeTargets.Property

--- Attributes/Program.cs
+++ Attributes/Program.cs
@@ -16,20 +16,48 @@ namespace Attributes
             Console.ReadLine();
         }
     }
+    [ToTable("Customers")]
+    //Attributelar ilgili nesne için kural oluşturmaya yarar
     class Customer
     {
         public int Id { get; set; }
-        //[RequiredProperty]
+        [RequiredProperty]
         public string FirstName { get; set; }
+        [RequiredProperty]
         public string LastName { get; set; }
+        [RequiredProperty]
         public int Age { get; set; }
     }
     class CustomerDal
-    {
+    { //Obsolete built-in bir attribute tur yapının artık kullanılmadığını söyler
+        [Obsolete("Dont use Add, use AddNew")]
         public void Add(Customer customer)
         {
             Console.WriteLine("{0}, {1}, {2}, {3} added!",
                 customer.Id, customer.FirstName, customer.LastName, customer.Age);
         }
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3} added!",
+                customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
     }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)] //attributeun hangi attributelarda kullanılabileceğini belirtir
+    class RequiredPropertyAttribute : Attribute //attributeların sonuna Attribute yazılır
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Class)]
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            this._tableName = tableName;
+        }
+    }
+
+
 }
