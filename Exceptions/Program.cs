21954075900174de5091abc9a3165f9a0969b3bf
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-04 20:08:57 +0300

    Func<int> with lambda

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exceptions
@@ -20,6 +21,19 @@ namespace Exceptions
 
             Func<int, int, int> add = Topla;
             Console.WriteLine(add(3, 5));
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+            
+
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 15);
+
+            Console.WriteLine(getRandomNumber());
+            //Thread.Sleep(1000);
+            Console.WriteLine(getRandomNumber2());
 
             Console.ReadLine();
         }
